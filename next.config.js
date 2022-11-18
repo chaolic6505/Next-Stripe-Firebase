ae26e1d41ce07b6a44418a28fe95caf508a68c81
Steven <chaolic6505@gmail.com>
2022-11-17 23:04:33 -0800

    update next config  to allow remote images source

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
+        domains: ['rb.gy', 'image.tmdb.org'],
         dangerouslyAllowSVG: true,
         contentSecurityPolicy:
             "default-src 'self'; script-src 'none'; sandbox;",
